39d28d4c737cb2eeed12811eedd940982762ffce
edrlu <edwardlu.042@gmail.com>
2026-01-01 23:03:47 -0800

    remv psd

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,15 +1,7 @@
-'use client'
-
-import PasswordProtection from '@/components/auth/PasswordProtection'
-
 export default function PortfolioLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <PasswordProtection>
-      {children}
-    </PasswordProtection>
-  )
+  return <>{children}</>
 }
